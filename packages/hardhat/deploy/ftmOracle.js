1e19196efffbee2789c9930e192d7309bb23629c
Ataxia123 <vp.ca94@gmail.com>
2022-05-16 06:27:25 -0500

    Added OneToken contracts.
    WIP: Oracle setup for vBTC
    
    TODO: Front end contracts on scaffold

---
+++ packages/hardhat/deploy/ftmOracle.js
@@ -0,0 +1,63 @@
+module.exports = async function({ ethers: { getNamedSigner }, getNamedAccounts, deployments }) {
+    const { deploy } = deployments
+
+    const wFtmAddr = "0x21be370d5312f44cb42ce377bc9b8a0cef1a4c83"
+
+    const indexTokenETH = "0x74b23882a30290451a17c44f4f05243b6b58c76d"
+
+    const uniSwapFactory = "0x152ee697f2e276fa89e96742e9bb9ab1f2e61be3"
+  
+    const { deployer, dev } = await getNamedAccounts()
+  
+    const chainId = await getChainId()
+
+    const moduleType = {
+        version: 0,
+        controller: 1,
+        strategy: 2,
+        mintMaster: 3,
+        oracle: 4,
+        voterRoll: 5
+    }
+
+    const 
+        name = "FTM Oracle"
+        url = "hhtps://strudel.finance"
+
+    if (chainId != 1) { //don't deploy to mainnet
+        const 
+            collateralToken = wFtmAddr //change to wBTC
+            factory = await deployments.get("OneTokenFactory")
+            Admin = await ethers.getContractFactory("OneTokenFactory")
+            admin = Admin.attach(factory.address)
+            rate = 1000
+
+        const oracle = await deploy("FTMOracle", { // wBTC:DAI
+            from: deployer,
+            args: [factory.address, uniSwapFactory, collateralToken, rate],
+            log: true
+        })
+
+        if (chainId != 31337) { //don't verify contract on localnet
+            await hre.run("verify:verify", {
+                address: oracle.address,
+                constructorArguments: [
+                    factory.address,
+                    uniSwapFactory,
+                    indexTokenETH,
+                    name,
+                    collateralToken
+                ],
+            })
+        }
+    
+        await admin.admitModule(oracle.address, moduleType.oracle, name, url, {
+            from: deployer
+        })
+    }
+    
+
+}
+
+module.exports.tags = ["testMemberTokenOracle","testToken"]
+module.exports.dependencies = ["testTokens","oneTokenFactory"]
---
+++ packages/hardhat/docs/interface/IController.md
@@ -0,0 +1,38 @@
+## `IController`
+
+
+
+
+
+
+### `oneTokenFactory() → address` (external)
+
+
+
+
+
+### `description() → string` (external)
+
+
+
+
+
+### `init()` (external)
+
+
+
+
+
+### `periodic()` (external)
+
+
+
+
+
+### `MODULE_TYPE() → bytes32` (external)
+
+
+
+
+
+
