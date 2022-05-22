b97889867044080967e7941d5dbcf0b6f2148da0
Ataxia123 <vp.ca94@gmail.com>
2022-05-22 07:09:42 -0500

    fixed subgraph logics

--- packages/subgraph/src/mapping.ts
+++ packages/subgraph/src/mapping.ts
@@ -1,9 +1,9 @@
-import  {Minter, MinterCounter, RedeemerCounter, TotalMitiSupply, TotalvBTCReserves, TotalUSDCReserves } from "../generated/schema";
+import  {Minter, MinterCounter, RedeemerCounter, TotalMitiSupply, TotalvBTCReserve, TotalUSDCReserve } from "../generated/schema";
 import  {Minted, Redeemed} from "../generated/OneTokenV1/OneTokenV1";
 import { BigInt } from '@graphprotocol/graph-ts';
 
 export function handleMinted(event: Minted): void {
-    let day = (event.block.timestamp / BigInt.fromI32(60*60*24))
+
     let minter = Minter.load(event.params.sender.toHex())
     if (minter == null) {
         // Minter
@@ -22,48 +22,43 @@ export function handleMinted(event: Minted): void {
             minterCounter.count = minterCounter.count + 1
         }
         minterCounter.save()
-}
-        let mitisupply = TotalMitiSupply.load(event.params.sender.toHex())
-        if (mitisupply == null) {
-            mitisupply = new TotalMitiSupply(event.params.sender.toHex())
-            mitisupply.totalSupply = event.params.oneTokens
-            mitisupply.totalMinted = event.params.oneTokens
-            mitisupply.totalBurned = BigInt.fromI32(0)
-        } else {
-            mitisupply.totalSupply = mitisupply.totalSupply + event.params.oneTokens
-            mitisupply.totalMinted = mitisupply.totalMinted + event.params.oneTokens
-        }
-        mitisupply.save()
-
-        let totalvBTCReserves = TotalvBTCReserves.load(event.params.sender.toHex())
-        if (totalvBTCReserves == null) {
-            totalvBTCReserves = new TotalvBTCReserves(event.params.sender.toHex())
-            totalvBTCReserves.totalDeposits = event.params.memberTokens
-        } else {
-            totalvBTCReserves.totalDeposits = totalvBTCReserves.totalDeposits + event.params.memberTokens
-
-        }
-        totalvBTCReserves.save()
-
-        let totalUSDCReserves = TotalUSDCReserves.load(event.params.sender.toHex())
-        if (totalUSDCReserves == null) {
-            totalUSDCReserves = new TotalUSDCReserves(event.params.sender.toHex())
-            totalUSDCReserves.totalMinted = event.params.collateralTokens
-            totalUSDCReserves.totalSupply = event.params.collateralTokens
-            totalUSDCReserves.totalBurned = BigInt.fromI32(0)
-        } else {
-            totalUSDCReserves.totalSupply = totalUSDCReserves.totalSupply + event.params.collateralTokens
-            totalUSDCReserves.totalMinted = totalUSDCReserves.totalMinted + event.params.collateralTokens
-        }
-        totalUSDCReserves.save()
-   
+    }
     minter.totalMinted = minter.totalMinted + event.params.oneTokens
-    minter.save() } 
+    minter.save()
 
+    // MitiSupply
+    let mitisupply = TotalMitiSupply.load(event.params.sender.toHex())
+    if (mitisupply == null) {
+        mitisupply = new TotalMitiSupply(event.params.sender.toHex())
+        mitisupply.totalSupply = BigInt.fromI32(0)
+        mitisupply.totalMinted = BigInt.fromI32(0)
+        mitisupply.totalBurned = BigInt.fromI32(0)
+    } 
+    mitisupply.totalSupply = mitisupply.totalSupply + event.params.oneTokens
+    mitisupply.save()
 
+    let totalvBTCReserve = TotalvBTCReserve.load(event.params.sender.toHex())
+    if (totalvBTCReserve == null) {
+        totalvBTCReserve = new TotalvBTCReserve(event.params.sender.toHex())
+        totalvBTCReserve.totalDeposits = BigInt.fromI32(0)
+        }
+    totalvBTCReserve.totalDeposits = totalvBTCReserve.totalDeposits + event.params.oneTokens
+    totalvBTCReserve.save()
+
+    let totalUSDCReserve = TotalUSDCReserve.load(event.params.sender.toHex())
+    if (totalUSDCReserve == null) {
+        totalUSDCReserve = new TotalUSDCReserve(event.params.sender.toHex())
+        totalUSDCReserve.totalSupply = BigInt.fromI32(0)
+        totalUSDCReserve.totalMinted = BigInt.fromI32(0)
+        totalUSDCReserve.totalBurned = BigInt.fromI32(0)
+    }
+    totalUSDCReserve.totalSupply = totalUSDCReserve.totalSupply + event.params.collateralTokens
+    totalUSDCReserve.totalMinted = totalUSDCReserve.totalMinted + event.params.collateralTokens
+    totalUSDCReserve.save()
+} // handleMinted
 
 export function handleRedeemed(event: Redeemed): void {
-    let day = (event.block.timestamp / BigInt.fromI32(60*60*24))
+
     let minter = Minter.load(event.params.sender.toHex())
     if (minter == null) {
         // Minter
@@ -74,37 +69,39 @@ export function handleRedeemed(event: Redeemed): void {
 
         // MinterCounter
 
-        let redeemerCounter = RedeemerCounter.load('singleton')
-        if (redeemerCounter == null) {
-            redeemerCounter = new RedeemerCounter('singleton')
-            redeemerCounter.count = 1
+        let minterCounter = MinterCounter.load('singleton')
+        if (minterCounter == null) {
+            minterCounter = new MinterCounter('singleton')
+            minterCounter.count = 1
         } else {
-            redeemerCounter.count = redeemerCounter.count + 1
+            minterCounter.count = minterCounter.count + 1
         }
-        redeemerCounter.save()
-
-        let mitisupply = TotalMitiSupply.load(event.params.sender.toHex())
-        if (mitisupply == null) {
-            mitisupply = new TotalMitiSupply(event.params.sender.toHex())
-            mitisupply.totalSupply = event.params.amount
-            mitisupply.totalMinted = event.params.amount
-            mitisupply.totalBurned = BigInt.fromI32(0)
-        } else {
-            mitisupply.totalSupply = mitisupply.totalSupply - event.params.amount
-            mitisupply.totalBurned = mitisupply.totalBurned + event.params.amount
-        }
-        mitisupply.save()
-
-        let totalUSDCReserves = TotalUSDCReserves.load(event.params.sender.toHex())
-        if (totalUSDCReserves == null) {
-            totalUSDCReserves = new TotalUSDCReserves(event.params.sender.toHex())
-            totalUSDCReserves.totalMinted = event.params.amount
-            totalUSDCReserves.totalBurned = BigInt.fromI32(0)
-        } else {
-            totalUSDCReserves.totalSupply = totalUSDCReserves.totalSupply - event.params.amount
-            totalUSDCReserves.totalBurned = totalUSDCReserves.totalBurned + event.params.amount
-        }
-        totalUSDCReserves.save()
+        minterCounter.save()
+    }
     minter.totalBurned = minter.totalBurned + event.params.amount
     minter.save()
-    }   }
+
+    // MitiSupply
+    let mitisupply = TotalMitiSupply.load(event.params.sender.toHex())
+    if (mitisupply == null) {
+        mitisupply = new TotalMitiSupply(event.params.sender.toHex())
+        mitisupply.totalSupply = BigInt.fromI32(0)
+        mitisupply.totalMinted = BigInt.fromI32(0)
+        mitisupply.totalBurned = BigInt.fromI32(0)
+    } 
+    mitisupply.totalSupply = mitisupply.totalSupply - event.params.amount
+    mitisupply.totalBurned = mitisupply.totalBurned + event.params.amount
+    mitisupply.save()
+
+    let totalUSDCReserve = TotalUSDCReserve.load(event.params.sender.toHex())
+    if (totalUSDCReserve == null) {
+        totalUSDCReserve = new TotalUSDCReserve(event.params.sender.toHex())
+        totalUSDCReserve.totalSupply = BigInt.fromI32(0)
+        totalUSDCReserve.totalMinted = BigInt.fromI32(0)
+        totalUSDCReserve.totalBurned = BigInt.fromI32(0)
+    }
+    totalUSDCReserve.totalSupply = totalUSDCReserve.totalSupply - event.params.amount
+    totalUSDCReserve.totalBurned = totalUSDCReserve.totalBurned + event.params.amount
+    totalUSDCReserve.save()
+} // handleRedeemed
+
